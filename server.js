ab78423b5c67c1e6f9ac0b3a8f9196a9bcf345ec
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-01-10 06:27:00 -0300

    alters

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const mqtt = require("mqtt");
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const compression = require("compression");
-const morgan = require("morgan");
+const mqtt = require('mqtt');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 3333;
-const client = mqtt.connect("https://mqtt.eclipseprojects.io/");
+const client = mqtt.connect('https://mqtt.eclipseprojects.io/');
 const condi = {};
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,32 +16,32 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
-client.on("connect", () => {
-  client.subscribe("DHT11_temperatura_pintura_teste01");
-  client.subscribe("DHT11_umidade_pintura_teste01");
-  client.subscribe("cron_ink_start");
+client.on('connect', () => {
+  client.subscribe('DHT11_temperatura_pintura_teste01');
+  client.subscribe('DHT11_umidade_pintura_teste01');
+  client.subscribe('cron_ink_start');
 });
 
-client.on("message", (topic, payload) => {
-  if (topic === "DHT11_temperatura_pintura_teste01") {
+client.on('message', (topic, payload) => {
+  if (topic === 'DHT11_temperatura_pintura_teste01') {
     condi.temperatura = payload.toString();
-  } else if (topic === "DHT11_umidade_pintura_teste01") {
+  } else if (topic === 'DHT11_umidade_pintura_teste01') {
     condi.umidade = payload.toString();
   }
-  app.get("/condicao", (req, res) => {
+  app.get('/condicao', (req, res) => {
     res.send(condi);
   });
 });
 
-app.post("/potlife", (req, res) => {
+app.post('/potlife', (req, res) => {
   const potlife = req.body.potlife.toString();
-  res.send("POST recebido com sucesso");
+  res.send('POST recebido com sucesso');
 
   if (potlife) {
     console.log(potlife);
-    client.publish("cron_ink_start", potlife);
+    client.publish('cron_ink_start', potlife);
   }
 });
 
