f926cbfb0d00bc454cced39cf3839e431f1c8a52
pedro prado <pedroh.prado@outlook.com>
2024-11-27 10:14:50 -0300

    add changes

--- src/controller/controllerTempAndUmi.js
+++ src/controller/controllerTempAndUmi.js
@@ -7,22 +7,29 @@ const { getTemperatureAndHumity03 } = require('../models/sensor03');
 const getTempAndUmi03 = async (req, res) => {
   try {
     const sensor03 = await getTemperatureAndHumity03();
-    console.log(sensor03)
 
-    res.status(200).json(sensor03);
+    if(sensor03.mensage === 'Não foi possível obter os dados em 10 segundos')
+      return res.status(400).json({message: 'erro ao conectar com sensores'})
+    else return res.status(200).json(sensor03);
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      error: 'Erro interno do servidor',
+    });
   }
 };
 
 const getTempAndUmi02 = async (req, res) => {
   try {
     const sensor02 = await getTemperatureAndHumity02();
-    console.log(sensor02)
-
-    res.status(200).json(sensor02);
+    if(sensor02.mensage === 'Não foi possível obter os dados em 10 segundos')
+      return res.status(400).json({message: 'erro ao conectar com sensores'})
+    else return res.status(200).json(sensor02);
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      error: 'Erro interno do servidor',
+    });
   }
 };
 
